5af53e2d56084f4114b7bdad419679554f667492
zephyr <zephyr@oacy.io>
2018-07-31 16:36:40 +0800

    dubbo demo

---
+++ dubboDemo/dubbo-api/src/main/java/io/oacy/dubbo/api/App.java
@@ -0,0 +1,13 @@
+package io.oacy.dubbo.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
